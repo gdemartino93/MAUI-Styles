5b5a2d2e58c3a8290b47b88cc4087b106198415a
gdemartino93 <g.demartino9393@gmail.com>
2025-03-22 17:43:57 +0100

    classes of style

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new DynamicStyleDemo();
+            MainPage = new StyleClassDemo();
         }
     }
 }
---
+++ StyleClassDemo.xaml.cs
@@ -0,0 +1,9 @@
+namespace MAUI_Styles;
+
+public partial class StyleClassDemo : ContentPage
+{
+	public StyleClassDemo()
+	{
+		InitializeComponent();
+	}
+}
